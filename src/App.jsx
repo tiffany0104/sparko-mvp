5a291d39d37b429dbc6cf2f5fc1c6e9fa055f91e
tiffany0104 <77959735+tiffany0104@users.noreply.github.com>
2025-06-01 17:05:44 +0100

    上傳文件

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Sparko MVP</h1>
+      <p>This is your starting page. Let's build something great!</p>
+    </div>
+  )
+}
+
+export default App
